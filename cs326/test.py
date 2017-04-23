429fdf3921595bc1456741cc81b68decd1bc072f
Nilay Thakor <nthakor@nd.edu>
2017-04-23 19:00:50 +0530

    completed set associative

---
+++ cs326/test.py
@@ -0,0 +1,10 @@
+s='s'
+A=[s, '0x1fffff60', 1]
+B=[s, '0x1fffff69', 1]
+C=[s, '0x1fffff62', 1]
+D=[s, '0x1fffff6b', 1]
+E=[s, '0x1fffff65', 1]
+F=[s, '0x1fffff6f', 1]
+X=[A,B,C,B,A,B,E,C,A,D,B,C,A,F,D,B,C,E,A]
+for x in X:
+	print x
